f4047bfc27ac9d1ed7940e0c6352d5a8aa013c98
Arnab Sen <azure3317@gmail.com>
2019-12-06 21:26:45 +1300

    Removed XAML

---
+++ CodeMap/MainWindow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CodeMap
+{
+    public class MyWindow : Window
+    {
+
+        public MyWindow()
+        {
+            Width = 300;
+            Height = 300;
+
+        }
+
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+
+            app.Run(new MyWindow());
+        }
+    }
+}
